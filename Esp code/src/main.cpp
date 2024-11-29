320cf4fee2dc998ec3f17dc93d1818f18e669c13
Giuseppe Magliano <91559694+root-over@users.noreply.github.com>
2024-11-30 00:38:30 +0100

    aggiunta ricezione mex uart e bugfix

--- Esp code/src/main.cpp
+++ Esp code/src/main.cpp
@@ -15,7 +15,6 @@ unsigned char rxBuf[8];
 unsigned long previousMillis = 0;  // Variabile per memorizzare l'ultimo tempo in cui il messaggio è stato inviato
 const long interval = 1000;        // Intervallo di tempo
 
-
 #define CAN0_INT 4                             
 MCP_CAN CAN0(5);                               
 
@@ -23,11 +22,37 @@ int Velocita = 0;
 int Rpm = 0;
 int Marcia = 0;
 
-// Funzione per inviare un messaggio via UART e verificare il successo
+// Funzione per inviare un messaggio via UART e verificare se il messaggio è stato inviato con successo
 bool sendUartMessage(const char* message) {
   int bytesSent = mySerial.print(message);
   return bytesSent == strlen(message);  // Ritorna true se il numero di byte inviati corrisponde alla lunghezza del messaggio
 }
+
+// Funzione per ricevere dati via UART
+void receiveUartData() {
+    if (mySerial.available() >= 6) {
+        uint8_t data[6];
+        mySerial.readBytes(data, 6);
+
+        // Verifica che il primo e l'ultimo elemento siano SOP e EOP
+        if (data[0] == SOP && data[5] == EOP) {
+            // Stampa gli elementi tra SOP e EOP
+            for (int i = 1; i < 5; i++) {
+                Serial.print(data[i]);
+              }
+                
+                Serial.print(" \n");
+                byte sndStat = CAN0.sendMsgBuf(0x999, 0, 6, data); // invio il messaggio via CAN
+                // TODO dal cluster leggere il messaggio ricevuto ed interpretarlo di conseguenza
+                if(sndStat == CAN_OK){
+                  Serial.println("Messagio CAN inviato alla macchina!");
+                } else {
+                  Serial.println("Errore invio messaggio CAN...");
+                }
+            }
+            Serial.println();
+        }
+    }
 
 void setup()
 {
@@ -35,18 +60,17 @@ void setup()
   mySerial.begin(9600);
   
   if(CAN0.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK)
-    Serial.println("MCP2515 Initialized Successfully!");
+    Serial.println("CAN Inizializzato");
   else
-    Serial.println("Error Initializing MCP2515...");
+    Serial.println("Errore con CAN...");
   
   CAN0.setMode(MCP_NORMAL);                     
   pinMode(CAN0_INT, INPUT);                    
-  
-  Serial.println("MCP2515 Library Receive Example...");
 }
 
 void loop()
 {
+  receiveUartData();
   unsigned long currentMillis = millis();
   const char* message = nullptr;
   if(!digitalRead(CAN0_INT))                   
@@ -56,9 +80,6 @@ void loop()
     if(rxId == 0x351) Velocita = int(rxBuf[1]);
     if(rxId == 0x359) Marcia = int(rxBuf[7]);
     if(rxId == 0x35B) Rpm = int(rxBuf[1]);
-
-    
-
     
     if (Marcia == 96) {
       message = "Incidente";
@@ -71,6 +92,7 @@ void loop()
     } 
     else if (Rpm > 0 && Velocita > 0) {
       message = "OK";
+      
     }
     else {
       message = "Lavori";
