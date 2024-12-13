860ab0a68740828654efbae72201bc46e72ca3b3
SabatinoRaffaele99 <80411714+SabatinoRaffaele99@users.noreply.github.com>
2024-12-13 20:54:30 +0100

    Create Car_message.h
    Scomposizione delle struct

---
+++ PaloRaf/Car_message.h
@@ -0,0 +1,12 @@
+#ifndef CAR_MESSAGE_H
+#define CAR_MESSAGE_H
+
+typedef struct Auto {
+	uint16_t autoid;
+	//bool traffico;
+	bool incidente;
+	//bool lavori_in_corso;
+	bool sos;
+} Auto;
+
+#endif
